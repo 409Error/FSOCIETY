9bbbc390b5ff2cb9d4ffcfc6be8e8d7311d4b713
Melnik Nikolai <lastsundaydigital@gmail.com>
2019-01-13 16:02:02 +0200

    new html and css

---
+++ mainjs/main.js
@@ -0,0 +1,9 @@
+// main registration on website	
+window.onload = function() {
+	var button = document.getElementById('butReg');
+	button.onclick = function(){
+		let form = document.querySelector('#reg');
+	}
+};
+
+
