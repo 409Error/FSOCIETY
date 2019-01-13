f296b11ae1e8e26b6e65ed6d50148f2fd44ed42e
Melnik Nikolai <lastsundaydigital@gmail.com>
2019-01-13 16:33:50 +0200

    a little bit js

--- mainjs/main.js
+++ mainjs/main.js
@@ -1,8 +1,14 @@
 // main registration on website	
 window.onload = function() {
-	var button = document.getElementById('butReg');
+	var button = document.querySelector('.inputRegistartion');
 	button.onclick = function(){
-		let form = document.querySelector('#reg');
+		let information = document.querySelector('form');
+		let newUser = new Object();
+		newUser.mobNumber = information[0].value;
+		newUser.name = information[1].value;
+		newUser.login = information[2].value;
+		newUser.password = information[3].value;
+		console.log(newUser)
 	}
 };
 
