5338a002d9aeba2768cc75159cdd76a9a7339331
Melnik Nikolai <lastsundaydigital@gmail.com>
2019-01-17 16:01:30 +0200

    some change

--- mainjs/main.js
+++ mainjs/main.js
@@ -3,13 +3,38 @@ window.onload = function() {
 	var button = document.querySelector('.inputRegistartion');
 	button.onclick = function(){
 		let information = document.querySelector('form');
-		let newUser = new Object();
-		newUser.mobNumber = information[0].value;
-		newUser.name = information[1].value;
-		newUser.login = information[2].value;
-		newUser.password = information[3].value;
-		console.log(newUser)
+		//a function for validation the form
+		if(information[0].value == '' || information[0].value == ' '){
+			information[0].style.borderColor = 'red'
+		}
+		else{
+			information[0].style.borderColor = 'green'
+		}
+		if(information[1].value == '' || information[1].value == ' '){
+			information[1].style.borderColor = 'red'
+		}
+		else{
+			information[1].style.borderColor = 'green'
+		}
+		if(information[2].value == '' || information[2].value == ' '){
+			information[2].style.borderColor = 'red'
+		}
+		else{
+			information[2].style.borderColor = 'green'
+		}
+		if(information[3].value == '' || information[3].value == ' ' || information[3].lenght > 6){
+			console.log('error')
+		}
+		else{
+			let newUser = new Object();
+			newUser.mobNumber = information[0].value;
+			newUser.name = information[1].value;
+			newUser.login = information[2].value;
+			newUser.password = information[3].value;
+			console.log(newUser)
+		}
 	}
 };
+
 
 
